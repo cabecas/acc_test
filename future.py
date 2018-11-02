c183b2cd2758136399aa150bfa3cd2fb79c210c1
FS <fernandorsilva9@gmail.com>
2018-11-02 10:34:53 +0000

    Trabalho no ficheiro de selecção dos valores todos para um dado pulso

--- future.py
+++ future.py
@@ -12,7 +12,7 @@ import acc
 import matplotlib.pyplot as plt
 
 
-def indices(mat, cut_value=1.5, pulse_size=600):
+def indices_max(mat, cut_value=1.5, pulse_size=600):
     # Both the cut_value and pulse_size should come from the .txt file.
     # Operator must be able to take them into the calculation;
     # Specially because different substrates lead to different variations
@@ -35,8 +35,32 @@ def indices(mat, cut_value=1.5, pulse_size=600):
 
     return maxim
 
+def indices_min(mat=slopes, cut_value=1.5, pulse_size=600):
+    # Both the cut_value and pulse_size should come from the .txt file.
+    # Operator must be able to take them into the calculation;
+    # Specially because different substrates lead to different variations
+    # and the time the pump is working varies with [S];
+    """
+    Goal:
+    Input1: slopes; the product of acc.declive
+    Input2:
+    Input3:
+    Output:
+    """
 
-def selecting_entire_pulse(mat, maxim):
+    pH = mat[:, 1]
+    pH_antes = pH[: -pulse_size]
+    pH_depois = pH[pulse_size:]
+
+    dif = pH_antes - pH_depois
+    dif = -dif
+
+    minim,_ = scipy.signal.find_peaks(dif, height = cut_value, distance = pulse_size)
+
+    return minim
+
+
+def selecting_entire_pulse(mat, vec = minim):
     # Both the cut_value and pulse_size should come from the .txt file.
     # Operator must be able to take them into the calculation;
     # Specially because different substrates lead to different variations
@@ -48,51 +72,75 @@ def selecting_entire_pulse(mat, maxim):
     Output:
     """
 
-    max_minus = np.array(np.zeros(maxim.shape[0]+1))
-
-    for i in np.arange(maxim.shape[0]):
-        max_minus[i] = maxim[i]-1
-
-    max_minus[(maxim.shape[0])] = mat.shape[0] # Number of pulses here
-    max_minus = max_minus[1:].astype(int)
+    '''
 
     t = []
     pH = []
 
     for i in np.arange(maxim.shape[0]):
-        t1 = mat[maxim[i]:max_minus[i], 0]
-        pH1 = mat[maxim[i]:max_minus[i], 1]
+        t1 = mat[maxim[i]:minim[i], 0]
+        pH1 = mat[maxim[i]:minim[i], 1]
 
         t = np.append(t1, t)
         pH = np.append(pH1, pH)
 
+    '''
+
+    max_minus = np.zeros(vec.shape[0]+1)
+
+    for i in np.arange(vec.shape[0]):
+        max_minus[i] = vec[i]-1
+
+    max_minus[(vec.shape[0])] = mat.shape[0] # Number of pulses here
+    max_minus = max_minus[1:].astype(int)
+
+    t = []
+    pH = []
+
+    for i in np.arange(vec.shape[0]):
+        t1 = mat[vec[i]:max_minus[i], 0]
+        pH1 = mat[vec[i]:max_minus[i], 1]
+
+        t = np.append(t, t1)
+        pH = np.append(pH, pH1)
+
+
     return t, pH
 
+def breaking_complete_pulse(t, pH):
+
+    all_pulses = []
+    pulse = []
+
+    for i in np.arange(pH.shape[0] - 1):
+        if pH[i] - pH[600] < -2:
+            pulse.append((t[i], pH[i]))
+
+        elif pulse != []:
+            AT = np.array(pulse)
+            AT[:, 0] = AT[:, 0] - AT[0, 0]
+            all_pulses.append(AT)
+            pulse = []
+
+    return all_pulses
+
+    pass
+
+maxim = indices_max(slopes)
+minim = indices_min(slopes)
+t, pH = selecting_entire_pulse(slopes)
+plt.plot(t, pH)
 
 
 
-'''
+
 mat = acc.read_file('pH412.test', 'Folha4')
 clean = acc.cleaning(mat, analise='pH')
 slopes = acc.declive(clean, window=100)
 
-t1,pH1 = select_entire_pulse(slopes, maxim)
+t1, pH1 = selecting_entire_pulse(slopes)
+
+
 all_pulses = acc.breaking(t1, pH1)
 #all_pulses = acc.breaking2(all_pulses)
 acc.plotting(all_pulses)
-'''
-
-'''
-pH = slopes[:, 1]
-pH_antes = pH[:-600]
-pH_depois = pH[600:]
-
-dif = pH_antes - pH_depois
-
-plt.plot(dif)
-plt.show()
-'''
-
-maxim = indices(slopes)
-t,pH = selecting_entire_pulse(slopes, maxim)
-plt.plot(t, pH)

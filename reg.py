95cdaa8dc33230106312f76cbfa02aac9fddb5ca
vilut9 <fernandorsilva9@gmail.com>
2018-12-08 09:16:26 +0000

    função return_parameter criada e está funcional para o ficheiro 412.xls

--- reg.py
+++ reg.py
@@ -8,7 +8,7 @@ def pulse_reg(lista):
 
 # Define 1st array of all_pulses as a variable A
     p1 = lista
-    print(p1)
+    #print(p1)                              # Data
 
 # Regression: A*np.exp(-k*x)+C
 #Adapt to fun.py
@@ -22,8 +22,8 @@ def pulse_reg(lista):
     plt.plot(p1[:,0], func(p1[:,0], *popt), 'r-',
         label='fit: A=%5.2f, k=%5.4f, C=%5.2f' % tuple(popt))
 
-    print(popt)
-    print(np.sqrt(np.diag(pcov)))
+    #print(popt)                            # Optimization parameters
+    #print(np.sqrt(np.diag(pcov)))          # Covariance matrix
 
     plt.xlabel('t (min)')
     plt.ylabel('pH')
@@ -32,4 +32,31 @@ def pulse_reg(lista):
 
     return popt
 
-b=pulse_reg(all_pulses[1])
+#b=pulse_reg(all_pulses[0])
+
+def return_parameter(pulses = all_pulses, parameter = 'k'):
+
+    all_list = [[] for i in range(len(pulses))]
+    k_pulse = [None]*3
+    n_list = [None]*len(pulses)
+
+    if parameter == 'A':
+        a = 0
+    elif parameter == 'k':
+        a = 1;
+    elif parameter == 'C':
+        a = 2;
+    else:
+        print("Please, writing one of the following options: "
+              "'A', 'k' or 'C'")
+
+    for i in range(len(pulses)):
+        k_pulse = pulse_reg(pulses[i])
+        all_list[i] = k_pulse
+        n_list[i] = k_pulse[a]
+
+    return n_list
+
+def plot_parameter(parameter_values):
+
+
